83d7336e677cd29f50b91c374adcdcaee3def4c1
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-10-15 23:03:47 -0300

    feat: add some pictures to home page
    - i really like photography

--- src/dev/lanjoni/views/home.cljs
+++ src/dev/lanjoni/views/home.cljs
@@ -21,4 +21,12 @@
            :className "underline hover:text-[#820CD1] transition duration-300"}
           "nubank")
          ", leveraging clojure to create seamless, user-focused banking experiences at one of the world's leading - and most innovative - fintech companies.")
-        (d/p "i really love mechanical keyboards, photography, functional programming and lisp.")))}))
+        (d/p "i really love mechanical keyboards, photography, functional programming and lisp.")
+        (d/figure
+         (d/img
+          {:src "/images/pictures/chopi_blackbird_at_pink_trumpet_tree.jpeg"
+           :alt "chopi blackbird at pink trumpet tree"
+           :className "w-full h-auto border-2 border-[#666666]"})
+         (d/figcaption
+          {:className "text-center text-md text-[#666666] mt-2"}
+          "chopi blackbird at pink trumpet tree"))))}))
