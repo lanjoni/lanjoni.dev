143a49d71df4b9f13721d02dea94f3fb85073c20
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2025-03-27 21:31:23 -0300

    feat: add base test and package-lock update

--- test/dev/lanjoni/core_test.cljs
+++ test/dev/lanjoni/core_test.cljs
@@ -1,5 +1,10 @@
 (ns dev.lanjoni.core-test
-  (:require [cljs.test :refer [deftest is]]))
+  (:require ["@testing-library/react" :as tlr]
+            [cljs.test :refer [deftest is]]
+            [helix.dom :as d]))
 
-(deftest a-test
-  (is (= 1 1)))
+(deftest a-component-test
+  (let [container (d/div "helix")
+        container (tlr/render container)
+        div       (.getByText container "helix")]
+    (is (= "helix" (.-textContent div)))))
